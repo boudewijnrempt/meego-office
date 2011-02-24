b3c762f471e72721b65c651349357c3d30a1295c
Girish Ramakrishnan <girish@forwardbias.in>
2011-02-25 00:38:58 +0530

    Add GroupBy support.
    ListView's section support is severely broken and we have to resort
    to all sorts of hacks. Current workaround is to change the value
    returned by section.property role.
    
    http://bugreports.qt.nokia.com/browse/QTBUG-17757
    http://bugreports.qt.nokia.com/browse/QTBUG-17758
    http://bugreports.qt.nokia.com/browse/QTBUG-17759

--- CMDocumentListModel.cpp
+++ CMDocumentListModel.cpp
@@ -16,31 +16,49 @@ SearchThread::~SearchThread()
 
 void SearchThread::run()
 {
+    // ## FIXME : Get this from the parts
+    static const char *docTypesText[] = {
+        QT_TRANSLATE_NOOP("CMDocumentListModel", "Words Document"),
+        QT_TRANSLATE_NOOP("CMDocumentListModel", "Stage Document"),
+        QT_TRANSLATE_NOOP("CMDocumentListModel", "Tables Document")
+    };
+    QHash<QString, QString> docTypes;
+    docTypes["odt"] = CMDocumentListModel::tr(docTypesText[0]);
+    docTypes["doc"] = CMDocumentListModel::tr(docTypesText[0]);
+    docTypes["odp"] = CMDocumentListModel::tr(docTypesText[1]);
+    docTypes["ppt"] = CMDocumentListModel::tr(docTypesText[1]);
+    docTypes["ods"] = CMDocumentListModel::tr(docTypesText[2]);
+    docTypes["xls"] = CMDocumentListModel::tr(docTypesText[2]);
+
     // ## FIXME: Among other things
     // 1. Entries must be sorted depending on the current sort
     // 2. Fetch metadata from the documents
-    // 3. nameFilters must query Calligra for supported document types
     QString documentsDir = QDesktopServices::storageLocation(QDesktopServices::DocumentsLocation);
     QStringList nameFilters;
-    nameFilters << "*.odt" << "*.doc" << "*.odp" << "*.ppt" << "*.xls";
+    for (QHash<QString, QString>::const_iterator it = docTypes.constBegin(); it != docTypes.constEnd(); ++it)
+        nameFilters.append("*." + it.key());
+
     QDirIterator it(documentsDir, nameFilters, QDir::Files, QDirIterator::Subdirectories);
     while (it.hasNext()) {
         it.next();
         CMDocumentListModel::DocumentInfo info;
         info.fileName = it.fileName();
         info.filePath = it.filePath();
+        info.docType = docTypes.value(info.fileName.right(3));
         emit documentFound(info);
     }
 }
 
 CMDocumentListModel::CMDocumentListModel(QObject *parent)
-    : QAbstractListModel(parent), m_searchThread(0)
+    : QAbstractListModel(parent), m_searchThread(0), m_groupBy(GroupByName)
 {
     qRegisterMetaType<DocumentInfo>();
 
     QHash<int, QByteArray> roleNames = QAbstractListModel::roleNames();
     roleNames[FileNameRole] = "fileName";
     roleNames[FilePathRole] = "filePath";
+    roleNames[DocTypeRole] = "docType";
+    roleNames[SectionCategoryRole] = "sectionCategory";
     setRoleNames(roleNames);
 }
 
@@ -60,10 +78,27 @@ void CMDocumentListModel::startSearch()
     QThreadPool::globalInstance()->start(m_searchThread);
 }
 
+static bool fileNameLessThan(const CMDocumentListModel::DocumentInfo &info1, const CMDocumentListModel::DocumentInfo &info2)
+{
+    return info1.fileName.toLower() < info2.fileName.toLower();
+}
+
+static bool docTypeLessThan(const CMDocumentListModel::DocumentInfo &info1, const CMDocumentListModel::DocumentInfo &info2)
+{
+    return info1.docType < info2.docType;
+}
+
 void CMDocumentListModel::addDocument(const DocumentInfo &info)
 {
-    beginInsertRows(QModelIndex(), m_documentInfos.count(), m_documentInfos.count());
-    m_documentInfos.append(info);
+    bool (*lessThanFunc)(const CMDocumentListModel::DocumentInfo &, const CMDocumentListModel::DocumentInfo &) = 0;
+    if (m_groupBy == GroupByName)
+        lessThanFunc = fileNameLessThan;
+    else
+        lessThanFunc = docTypeLessThan;
+    QList<DocumentInfo>::iterator it = qLowerBound(m_documentInfos.begin(), m_documentInfos.end(), info, fileNameLessThan);
+    const int pos = it - m_documentInfos.begin();
+    beginInsertRows(QModelIndex(), pos, pos);
+    m_documentInfos.insert(it, info);
     endInsertRows();
 }
 
@@ -91,6 +126,9 @@ QVariant CMDocumentListModel::data(const QModelIndex &index, int role) const
     case FileNameRole: // intentional fall through
     case Qt::DisplayRole: return info.fileName;
     case FilePathRole: return info.filePath;
+    case DocTypeRole: return info.docType;
+    case SectionCategoryRole: 
+        return m_groupBy == GroupByName ? info.fileName[0].toUpper() : info.docType;
     default: return QVariant();
     }
 }
@@ -103,5 +141,36 @@ QVariant CMDocumentListModel::headerData(int section, Qt::Orientation orientatio
     case 0: return tr("Filename");
     default: return QVariant();
     }
+}
+
+void CMDocumentListModel::groupBy(int role)
+{
+    if (m_groupBy == role)
+        return;
+    m_groupBy = role;
+    relayout();
+}
+
+void CMDocumentListModel::relayout()
+{
+    emit layoutAboutToBeChanged();
+    QMap<QString, QList<int> > map;
+    for (int i = 0; i < m_documentInfos.count(); i++) {
+        QString section;
+        if (m_groupBy == GroupByName)
+            section = m_documentInfos[i].fileName[0].toUpper();
+        else
+            section = m_documentInfos[i].docType;
+        map[section].append(i);
+    }
+    QList<DocumentInfo> newList;
+    for (QMap<QString, QList<int> >::const_iterator it = map.constBegin(); it != map.constEnd(); ++it) {
+        QList<int> indices = it.value();
+        foreach(int index, indices) {
+            newList.append(m_documentInfos[index]);
+        }
+    }
+    m_documentInfos = newList;
+    emit layoutChanged();
 }
 
--- CMDocumentListModel.h
+++ CMDocumentListModel.h
@@ -15,12 +15,15 @@ public:
 
     enum CustomRoles {
         FileNameRole = Qt::UserRole + 1,
-        FilePathRole
+        FilePathRole,
+        DocTypeRole,
+        SectionCategoryRole
     };
 
     struct DocumentInfo {
         QString filePath;
         QString fileName;
+        QString docType;
     };
 
     // reimp
@@ -33,9 +36,16 @@ public slots:
     void startSearch();
     void addDocument(const CMDocumentListModel::DocumentInfo &info);
 
+public:
+    Q_INVOKABLE void groupBy(int role);
+
 private:
+    void relayout();
+
     QList<DocumentInfo> m_documentInfos;
     SearchThread *m_searchThread;
+    enum { GroupByName, GroupByDocType };
+    int m_groupBy;
 };
 
 Q_DECLARE_METATYPE(CMDocumentListModel::DocumentInfo);

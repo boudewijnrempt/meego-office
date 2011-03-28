150e193edb16843b50981910f1f7db18d51b181f
Boudewijn Rempt <boud@valdyas.org>
2011-03-28 13:43:39 +0200

    show shapes on a stage page
    Stage is a bit complicated and needs to manually add the shapes to the
    shape manager.

--- stage/CMStageDeclarativeView.cpp
+++ stage/CMStageDeclarativeView.cpp
@@ -5,12 +5,24 @@
 #include <KoPAPage.h>
 #include <KoPADocument.h>
 #include <KoPACanvasBase.h>
+#include <KoZoomController.h>
+#include <KoShapeManager.h>
+#include <KoPACanvasItem.h>
+#include <kactioncollection.h>
+#include <KoSelection.h>
+#include <KoPAMasterPage.h>
+#include <KoShapeLayer.h>
 
 class CMStageDeclarativeView::Private
 {
 public:
     Private()
         : page(0)
+        , zoomController(0)
+        , doc(0)
+        , canvas(0)
+        , viewMode(0)
+        , actionCollection(0)
     { }
     ~Private() { }
 
@@ -18,19 +30,37 @@ public:
 
     KoZoomController* zoomController;
     KoPADocument* doc;
-    KoPACanvasBase* canvas;
+    KoPACanvasItem* canvas;
     KoPAViewModeNormal* viewMode;
+
+    KActionCollection *actionCollection;
 };
 
-CMStageDeclarativeView::CMStageDeclarativeView(KoZoomController* zoomController, KoPADocument* doc, KoPACanvasBase* canvas)
-    : d(new Private)
+CMStageDeclarativeView::CMStageDeclarativeView(KoZoomController* zoomController,
+                                               KoPADocument* doc,
+                                               KoPACanvasItem* canvas)
+    : QObject(0)
+    , d(new Private)
 {
     d->zoomController = zoomController;
     d->doc = doc;
-    d->canvas = canvas;
-    d->canvas->setView(this);
 
-    d->viewMode = new KoPAViewModeNormal(this, canvas);
+    d->canvas = canvas;
+    Q_ASSERT(d->canvas);
+    Q_ASSERT(d->canvas->resourceManager());
+
+    d->canvas->setAttribute(Qt::WA_OpaquePaintEvent, true);
+    d->canvas->setAutoFillBackground(false);
+
+    d->canvas->setView(this);
+    d->actionCollection = new KActionCollection(this);
+
+    d->actionCollection->addAction(KStandardAction::Prior, "page_previous", this, SLOT(previousPage()));
+    d->actionCollection->addAction(KStandardAction::Next, "page_next", this, SLOT(nextPage()));
+    d->actionCollection->addAction(KStandardAction::FirstPage, "page_first", this, SLOT(firstPage()));
+    d->actionCollection->addAction(KStandardAction::LastPage, "page_last", this, SLOT(lastPage()));
+
+    d->viewMode = new KoPAViewModeNormal(this, d->canvas);
     setViewMode(d->viewMode);
 }
 
@@ -39,6 +69,45 @@ CMStageDeclarativeView::~CMStageDeclarativeView()
     delete d;
 }
 
+KoPADocument* CMStageDeclarativeView::kopaDocument() const
+{
+    return d->doc;
+}
+
+KoZoomController* CMStageDeclarativeView::zoomController() const
+{
+    return d->zoomController;
+}
+
+void CMStageDeclarativeView::doUpdateActivePage(KoPAPageBase* page)
+{
+    bool activePageChanged = page != d->page;
+
+    setActivePage(page);
+
+    d->canvas->updateSize();
+    KoPageLayout &layout = page->pageLayout();
+
+    QSizeF pageSize(layout.width, layout.height);
+
+    d->canvas->setDocumentOrigin(QPointF(0,0));
+    // the page is in the center of the canvas
+    d->zoomController->setDocumentSize(pageSize);
+    d->zoomController->setPageSize(pageSize);
+
+    d->canvas->resourceManager()->setResource(KoCanvasResource::PageSize, pageSize);
+    d->canvas->update();
+
+    updatePageNavigationActions();
+
+    if (activePageChanged) {
+        proxyObject->emitActivePageChanged();
+    }
+    d->canvas->updateSize();
+}
+
+
+
 void CMStageDeclarativeView::setShowRulers(bool show)
 {
     Q_UNUSED(show);
@@ -46,22 +115,26 @@ void CMStageDeclarativeView::setShowRulers(bool show)
 
 void CMStageDeclarativeView::editPaste()
 {
-
 }
 
 void CMStageDeclarativeView::pagePaste()
 {
-
 }
 
 void CMStageDeclarativeView::insertPage()
 {
-
 }
 
 void CMStageDeclarativeView::updatePageNavigationActions()
 {
+    KoPADocument *doc = qobject_cast<KoPADocument*>(d->doc);
+    int index = doc->pageIndex(activePage());
+    int pageCount = doc->pages(viewMode()->masterMode()).count();
 
+    d->actionCollection->action("page_previous")->setEnabled(index > 0);
+    d->actionCollection->action("page_first")->setEnabled(index > 0);
+    d->actionCollection->action("page_next")->setEnabled(index < pageCount - 1);
+    d->actionCollection->action("page_last")->setEnabled(index < pageCount - 1);
 }
 
 void CMStageDeclarativeView::setActionEnabled(int actions, bool enable)
@@ -72,7 +145,12 @@ void CMStageDeclarativeView::setActionEnabled(int actions, bool enable)
 
 void CMStageDeclarativeView::navigatePage(KoPageApp::PageNavigation pageNavigation)
 {
-    Q_UNUSED(pageNavigation)
+    KoPADocument *doc = qobject_cast<KoPADocument*>(d->doc);
+    KoPAPageBase *newPage = doc->pageByNavigation(d->page, pageNavigation);
+
+    if (newPage != d->page) {
+        proxyObject->updateActivePage(newPage);
+    }
 }
 
 KoPAPageBase* CMStageDeclarativeView::activePage() const
@@ -82,27 +160,78 @@ KoPAPageBase* CMStageDeclarativeView::activePage() const
 
 void CMStageDeclarativeView::setActivePage(KoPAPageBase* page)
 {
+    if (!page) {
+        return;
+    }
+    if (d->page) {
+        d->canvas->shapeManager()->removeAdditional(d->page);
+    }
+
     d->page = page;
+
+    d->canvas->shapeManager()->addAdditional(d->page);
+
+    QList<KoShape*> shapes = page->shapes();
+    d->canvas->shapeManager()->setShapes(shapes, KoShapeManager::AddWithoutRepaint);
+
+    // Make the top most layer active
+    if(!shapes.isEmpty()) {
+        KoShapeLayer* layer = dynamic_cast<KoShapeLayer*>(shapes.last());
+        d->canvas->shapeManager()->selection()->setActiveLayer(layer);
+    }
+
+    // if the page is not a master page itself, set shapes of the master page
+    KoPAPage * paPage = dynamic_cast<KoPAPage *>(page);
+
+    if (paPage) {
+        KoPAMasterPage *masterPage = paPage->masterPage();
+        QList<KoShape*> masterShapes = masterPage->shapes();
+        d->canvas->masterShapeManager()->setShapes(masterShapes,
+                                                      KoShapeManager::AddWithoutRepaint);
+        // Make the top most layer active
+        if (!masterShapes.isEmpty()) {
+            KoShapeLayer* layer = dynamic_cast<KoShapeLayer*>(masterShapes.last());
+            d->canvas->masterShapeManager()->selection()->setActiveLayer(layer);
+        }
+    }
+    else {
+        // if the page is a master page no shapes are in the masterShapeManager
+        d->canvas->masterShapeManager()->setShapes(QList<KoShape*>());
+    }
+
+    // Set the current page number in the canvas resource provider
+    KoPADocument *doc = qobject_cast<KoPADocument*>(d->doc);
+    if (doc) {
+        d->canvas->resourceManager()->setResource(KoCanvasResource::CurrentPage,
+                                                     doc->pageIndex(d->page) + 1);
+    }
+
 }
 
-void CMStageDeclarativeView::doUpdateActivePage(KoPAPageBase* page)
-{
-    d->page = page;
-    d->page->update();
-}
 
-KoZoomController* CMStageDeclarativeView::zoomController() const
-{
-    return d->zoomController;
-}
-
-KoPADocument* CMStageDeclarativeView::kopaDocument() const
-{
-    return d->doc;
-}
 
 KoPACanvasBase* CMStageDeclarativeView::kopaCanvas() const
 {
     return d->canvas;
 }
 
+
+void CMStageDeclarativeView::previousPage()
+{
+    navigatePage(KoPageApp::PagePrevious);
+}
+
+void CMStageDeclarativeView::nextPage()
+{
+    navigatePage(KoPageApp::PageNext);
+}
+
+void CMStageDeclarativeView::firstPage()
+{
+    navigatePage(KoPageApp::PageFirst);
+}
+
+void CMStageDeclarativeView::lastPage()
+{
+    navigatePage(KoPageApp::PageLast);
+}
--- stage/CMStageDeclarativeView.h
+++ stage/CMStageDeclarativeView.h
@@ -2,27 +2,39 @@
 #define CMSTAGEDECLARATIVEVIEW_H
 
 #include <KoPAViewBase.h>
+#include <QObject>
 
+class KoPACanvasItem;
 
-class CMStageDeclarativeView : public KoPAViewBase
+class CMStageDeclarativeView : public QObject, public KoPAViewBase
 {
+    Q_OBJECT
 public:
-    explicit CMStageDeclarativeView(KoZoomController* zoomController, KoPADocument* doc, KoPACanvasBase* canvas);
+    explicit CMStageDeclarativeView(KoZoomController* zoomController,
+                                    KoPADocument* doc,
+                                    KoPACanvasItem* canvas);
     virtual ~CMStageDeclarativeView();
-    
-    virtual void setShowRulers(bool show);
-    virtual void editPaste();
-    virtual void pagePaste();
-    virtual void insertPage();
-    virtual void updatePageNavigationActions();
-    virtual void setActionEnabled(int actions, bool enable);
-    virtual void navigatePage(KoPageApp::PageNavigation pageNavigation);
-    virtual KoPAPageBase* activePage() const;
-    virtual void setActivePage(KoPAPageBase* page);
-    virtual void doUpdateActivePage(KoPAPageBase* page);
-    virtual KoZoomController* zoomController() const;
+
     virtual KoPADocument* kopaDocument() const;
     virtual KoPACanvasBase* kopaCanvas() const;
+    virtual KoZoomController* zoomController() const;
+    virtual void doUpdateActivePage(KoPAPageBase* page);
+    virtual void setActivePage(KoPAPageBase* page);
+    virtual KoPAPageBase* activePage() const;
+    virtual void navigatePage(KoPageApp::PageNavigation pageNavigation);
+    virtual void setActionEnabled(int actions, bool enable);
+    virtual void updatePageNavigationActions();
+    virtual void insertPage();
+    virtual void pagePaste();
+    virtual void editPaste();
+    virtual void setShowRulers(bool show);
+
+private slots:
+
+    void previousPage();
+    void nextPage();
+    void firstPage();
+    void lastPage();
 
 private:
     class Private;
